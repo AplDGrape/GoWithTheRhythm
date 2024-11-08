d5c79e85decc3c5bc542e2ef30c40e24f92d1214
AplDGrape <61457409+AplDGrape@users.noreply.github.com>
2024-11-08 13:54:46 +0800

    Added Void Temp Sprite Asset, Added Void in Gameplay Scene
    Currently just a decorative right now

---
+++ Go with the Rhythm/Assets/Scripts/VoidFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class VoidFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //position of camera - position of player
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Follows player indefinitely
+        //transform.position = player.position + offset;
+        //Follow player like a endless runner type game
+        Vector3 targetPos = player.position + offset;
+        targetPos.x = 0;
+        targetPos.y = 5;
+        transform.position = targetPos;
+    }
+}
