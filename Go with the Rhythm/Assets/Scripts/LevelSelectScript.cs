58e96fb3e8a7161dc91a0714520b598852a8e1cc
AplDGrape <61457409+AplDGrape@users.noreply.github.com>
2024-12-04 18:10:41 +0800

    Added Some Level Select Assets, Update Level Select, added Lean Tween Package for UI animation

--- Go with the Rhythm/Assets/Scripts/LevelSelectScript.cs
+++ Go with the Rhythm/Assets/Scripts/LevelSelectScript.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class LevelSelectScript : MonoBehaviour
+public class LevelSelectScript : MonoBehaviour, IEndDragHandler
 {
     //Get name of the level to select
     public string levelName;
+
+    [SerializeField] int maxPage;
+    int currentPage;
+    Vector3 targetPos;
+    [SerializeField] Vector3 pageStep;
+    [SerializeField] RectTransform levelPagesRect;
+
+    [SerializeField] float tweenTime;
+    [SerializeField] LeanTweenType tweenType;
+    float dragThreshould;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +31,65 @@ public class LevelSelectScript : MonoBehaviour
         
     }
 
+    private void Awake()
+    {
+        currentPage = 1;
+        targetPos = levelPagesRect.localPosition;
+        dragThreshould = Screen.width / 15;
+    }
+
+    public void Next()
+    {
+        if (currentPage < maxPage)
+        {
+            currentPage++;
+            targetPos += pageStep;
+            MovePage();
+        }
+    }
+
+    public void Previous()
+    {
+        if (currentPage > 1)
+        {
+            currentPage--;
+            targetPos -= pageStep;
+            MovePage();
+        }
+    }
+
+    void MovePage()
+    {
+        levelPagesRect.LeanMoveLocal(targetPos, tweenTime).setEase(tweenType);
+    }
+
     public void backScene()
     {
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void ChangeScene()
+    public void ElectricTherapyScene()
     {
-        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene("ElectricTherapy");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //If page swipe more than dragThresould -> page change
+        if (Mathf.Abs(eventData.position.x - eventData.pressPosition.x) > dragThreshould)
+        {
+            if (eventData.position.x > eventData.pressPosition.x)
+            {
+                Previous();
+            }
+            else
+            { 
+                Next();
+            }
+        }
+        else
+        {
+            MovePage();
+        }
     }
 }
